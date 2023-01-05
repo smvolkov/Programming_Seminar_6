f37cf134085ceebb244b75ef0c044d65bd712832
Semion Volkov <semen_volkov@mail.ru>
2023-01-05 14:03:04 +0500

    Task1 added

--- Seminar6.cs
+++ Seminar6.cs
@@ -28,4 +28,27 @@ int QtyRequest()
     return qty;
 }
 
-PositiveNumbers(QtyRequest());
+//PositiveNumbers(QtyRequest());
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void LinesCross(double b1, double k1, double b2, double k2)
+{
+    double x0 = (b2 - b1) / (k1 - k2);
+    double y0 = k1 * x0 + b1;
+
+    Console.WriteLine($"({x0}; {y0})");
+}
+
+double Coefficients(string coeffName)
+{
+    Console.WriteLine($"Введите коеффициент {coeffName}: ");
+    double coeff = double.Parse(Console.ReadLine());
+
+    return coeff;
+}
+
+LinesCross(Coefficients("b1"), Coefficients("k1"), Coefficients("b2"), Coefficients("k2"));
